11075bf39df075d663f74d6039ae0fa14b5cc0c6
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-18 13:02:20 -0700

    Working on first on part of further study

--- madlibs.py
+++ madlibs.py
@@ -46,10 +46,11 @@ def show_madlib_form():
     """Ask user if they want to paly the game."""
 
     get_answer = request.args.get("wants_game")
+    get_item = request.args.get("item")
     print request.args
 
     if get_answer == 'True':
-        return render_template("game.html")
+        return render_template("game.html", compliments=AWESOMENESS, item=get_item)
     elif get_answer == 'False':
         return render_template("goodbye.html")
     
@@ -63,11 +64,14 @@ def show_madlib():
     person = request.args.get("person")
     color = request.args.get("color")
     noun = request.args.get("noun")
-    adjective = request.args.get("adjective")
+    comp_type = request.args.get("comp_type")
+    compliment = request.args.get("compliment")
+    number = request.args.get("number")
+    
 
 
     return render_template("madlib.html", person=person, color=color,
-                                        noun=noun, adjective=adjective)
+                                        noun=noun, number=number, compliment=compliment)
 
 
 if __name__ == '__main__':
